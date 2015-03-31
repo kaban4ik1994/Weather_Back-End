135e5fb1c78656fdb18152c178245e5482ca750c
kaban4ik1994 <kaban4ik19901990@mail.ru>
2015-03-31 22:24:39 +0300

    The weather module was created.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var baseUrlApiGeotargeting = "http://maps.googleapis.com/maps/api/geocode/json";
 var express = require('express');
 var app = express();
 var request = require('request');
+var weather = require('./weather');
 
 app.get('/Weather', function(req, res){
 	res.header("Content-Type", "application/json");
@@ -16,7 +17,8 @@ app.get('/Weather', function(req, res){
 		q: req.query.latitude + ',' + req.query.longitude,
 		date: req.query.date }
 
-	request({url: baseUrlApiWeather, qs: requestParams}, function(error, response, body){
+	weather(baseUrlApiWeather, req.query.latitude + ',' + req.query.longitude, req.query.date,
+     function(error, response, body){
 		if (!error && response.statusCode == 200) {
 			res.send(body); }
 		else {
---
+++ weather.js
@@ -0,0 +1,14 @@
+var request = require('request');
+
+var weather = function(url, q, date, callbackFunction){
+		var requestParams = { 
+		key: '70388b130b191be8c6a64da274a27',
+		format: 'json',
+		num_of_days: 1,
+		q: q,
+		date: date }
+
+		request({url: url, qs: requestParams}, callbackFunction);
+}
+
+module.exports = weather;
