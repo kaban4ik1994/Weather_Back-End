342f3741e424c3c20554e1fa9eae52b723c0aea7
kaban4ik1994 <kaban4ik19901990@mail.ru>
2015-04-09 23:50:16 +0300

    Code review.

--- geotargeting.js
+++ geotargeting.js
@@ -1,13 +1,14 @@
 var baseUrlApiGeotargeting = 'http://maps.googleapis.com/maps/api/geocode/json';
 var request = require('request');
 
-var geotargeting = function(latlng, callbackFunction){
-		var requestParams = {
-		latlng: latlng,
-		language: 'EN', 
-		sensor: false };
+var geotargeting = function (options) {
+    var requestParams = {
+        latlng: options.latlng,
+        language: 'EN',
+        sensor: false
+    };
 
-		request({url: baseUrlApiGeotargeting, qs: requestParams}, callbackFunction);
+    request({ url: baseUrlApiGeotargeting, qs: requestParams }, options.callbackFunction);
 };
 
 module.exports = geotargeting;
--- server.js
+++ server.js
@@ -5,35 +5,44 @@ var request = require('request');
 var weather = require('./weather');
 var geotargeting = require('./geotargeting');
 
-function setResHeader(req, res, next){
-	res.header('Content-Type", "application/json');
-	res.header('Access-Control-Allow-Origin', '*');
+function setResHeader(req, res, next) {
+    res.header('Content-Type", "application/json');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-	next();
+    next();
 }
 
 app.get('/*', setResHeader);
 
-app.get('/Weather', function(req, res){
-	weather(req.query.latitude + ',' + req.query.longitude, req.query.date, '70388b130b191be8c6a64da274a27',
-     function(error, response, body){
-		if (!error && response.statusCode === 200) {
-			res.send(body); }
-		else {
-			res.status(400).send('Error');
-		}
-	});
+app.get('/Weather', function (req, res) {
+    weather({
+        key: '70388b130b191be8c6a64da274a27',
+        q: req.query.latitude + ',' + req.query.longitude,
+        date: req.query.date,
+        callbackFunction: function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                res.send(body);
+            }
+            else {
+                res.status(400).send('Error');
+            }
+        }
+    });
 });
 
-app.get('/Location', function(req, res){
-	geotargeting(req.query.latitude + ',' + req.query.longitude,
-	 function(error, response, body){
-    		if (!error && response.statusCode === 200) {
-    		res.send(body); }
-    		else{
-    			res.status(400).send('Error');
-    		}
-	});
+app.get('/Location', function (req, res) {
+    geotargeting({
+        latlng: req.query.latitude + ',' + req.query.longitude,
+        callbackFunction: function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                res.send(body);
+            }
+            else {
+                res.status(400).send('Error');
+            }
+        }
+    }
+);
 });
 
 app.listen(1337);
